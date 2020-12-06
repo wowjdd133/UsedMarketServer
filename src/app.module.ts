b72d27ff5d9fc8b9c6ca92fc6bf8f56ebdb6673b
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2020-12-06 21:15:56 +0900

    User structer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,43 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { User } from './user/entities/user.entity';
+import { Book } from './book/entities/book.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, 
+    BookModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_USERNAME: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Book],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,62 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { InternalServerErrorException } from '@nestjs/common';
+import { ObjectType, Field, Int, PickType } from '@nestjs/graphql';
+import { IsEmail, IsNumber, IsString, IsUUID } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Entity()
+export class User extends CoreEntity {
+  @Field(_ => String)
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Field(_ => String)
+  @Column()
+  @IsString()
+  password: string;
+
+  @Field(_ => String)
+  @Column()
+  @IsString()
+  name: string;
+
+  @Field(_ => String)
+  @Column({name: 'phone_number'})
+  @IsString()
+  phoneNumber: string;
+
+  @Column({default: false})
+  @Field(_ => Boolean)
+  verified: boolean;
+
+  @Field(_ => Int)
+  @Column()
+  @IsNumber()
+  sigunCode: number;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword():Promise<void> {
+      if(this.password){
+          try{
+              this.password = await bcrypt.hash(this.password, 10);
+          }catch(err){
+              console.log(err);
+              throw new InternalServerErrorException()
+          }    
+      }
+  }
+
+  async checkPassword(aPassword: string):Promise<boolean> {
+      try {
+          const ok = await bcrypt.compare(aPassword, this.password);
+          return ok;
+      }catch (err){
+          console.log(err);
+          throw new InternalServerErrorException();
+      }
+  }
 }
