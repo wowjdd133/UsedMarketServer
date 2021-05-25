6fb9ae0b33dcceb363fa2871ff60ff168497cf79
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2021-05-25 12:20:20 +0900

    sms aws -> nhn으로 변경

--- src/sns/sns.service.ts
+++ src/sns/sns.service.ts
@@ -5,64 +5,55 @@ import * as AWS from 'aws-sdk'
 import { ConfigService } from '@nestjs/config';
 import { SNS } from 'aws-sdk';
 import { PublishResponse } from 'aws-sdk/clients/sns';
+import axios from 'axios';
+import { ErrorStatus } from 'src/common/enums/errorStatus.enum';
 
 @Injectable()
 export class SnsService {
 
-    private readonly _sns: AWS.SNS;
-
     constructor(
         private readonly prisma:PrismaService,
         private readonly configService:ConfigService
-    ) {
-       AWS.config.update({
-           credentials: {
-               accessKeyId: configService.get("AWS_SNS_ACCESS_ID"),
-               secretAccessKey: configService.get("AWS_SNS_SECRET_KEY")
-           },
-           region: configService.get("AWS_SNS_REGION")
-       }) 
-
-       console.log()
-
-       this._sns = new SNS({
-           apiVersion: '2010-03-31',  
-       })
-    }
+    ) {}
 
     async sendCode({deviceId, phoneNumber}:sendCodeDto) {
         try {
             let code = Math.floor(Math.random() * 10000)+1000;
             if(code >= 10000) code -= 1000;
 
-            const korPhoneNumber = "82" + phoneNumber;
+            const now = new Date();
 
-            return this._sns
-                .publish({
-                    Message: code.toString(),
-                    PhoneNumber: korPhoneNumber,
-                })
-                .promise()
-                .then(async (info: PublishResponse) => {
+            const data = await axios.post(`https://api-sms.cloud.toast.com/sms/v2.4/appKeys/${this.configService.get('NHN_SMS_APP_KEY')}/sender/sms`, {
+                body: `테스트 해볼게영 마켓 인증 번호는 ${code}입니다.`,
+                sendNo: this.configService.get("MY_PHONE_NUMBER"),
+                requestDate: `${now.getFullYear()}-${`0${now.getMonth() + 1}`.slice(-2)}-${`0${now.getDate()}`.slice(-2)} ${now.getHours()}`,
+                recipientList: [{
+                    recipienNo: phoneNumber,
+                    countryCode: "82",
+                    internationalRecipientNo: "82" + phoneNumber,
+                }],
+                userId: "admin"
+            }, {
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8'
+                }
+            })
 
-                    console.log(info);
-
+            if(data.data) {
+                if(data.data.header.isSuccessful) {
                     await this.createVerifiation(deviceId, code);
+                }
 
-                    return {
-                        status: HttpStatus.OK,
-                        message: "메시지 전송 성공",
-                        data: info
-                    }
-                })
-                .catch((err) => {
-                    console.error(err);
+                //유효하지 않은 휴대폰 번호 = -2019
+                if(data.data.header.resultCode === -2019) {
                     throw new HttpException({
-                        status: HttpStatus.BAD_REQUEST,
-                        message: '메시지 전송 실패',
-                        data: err
+                        status: ErrorStatus.PHONE_NUMBER_NOT_VALID,
+                        message: "폰 번호가 유효하지 않습니다."
                     }, HttpStatus.BAD_REQUEST)
-                })
+                }
+            }
+
+            throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (err) {
             console.error(err);
             throw err;
